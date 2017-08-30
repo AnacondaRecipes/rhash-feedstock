80b934b3b92a4423e1882ebcd8c6c6e79691d3c3
Ray Donnelly <mingw.android@gmail.com>
2017-08-30 16:13:31 +0100

    here is no install rule in the librhash Makefile

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,13 @@
 if [[ ${HOST} =~ .*linux.* ]]; then
   make ADDCFLAGS="${CFLAGS}" ADDLDFLAGS="${LDFLAGS}" install-lib-static
   make             PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}" install-shared
-  make -C librhash PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}" install install-lib-shared install-lib-static
+  make -C librhash PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}"         install-lib-shared install-lib-static
 elif [[ ${HOST} =~ .*darwin.* ]]; then
   make             PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}" install                    install-lib-static
   make -C librhash PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}"                     dylib
   cp librhash/*.dylib* ${PREFIX}/lib
 fi
 
-cd tests
-./test_rhash.sh
+pushd tests
+  ./test_rhash.sh
+popd
