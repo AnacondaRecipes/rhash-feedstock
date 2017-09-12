a4295e6a6741295b2e5f3d41c22884fee0a31e28
Ray Donnelly <mingw.android@gmail.com>
2017-09-11 18:48:19 -0700

    Fix macOS dylib symlink and id name

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,9 @@ if [[ ${HOST} =~ .*linux.* ]]; then
 elif [[ ${HOST} =~ .*darwin.* ]]; then
   make             PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}" install                    install-lib-static
   make -C librhash PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}"                     dylib
-  cp librhash/*.dylib* ${PREFIX}/lib
+  cp -a librhash/*.dylib* ${PREFIX}/lib
+  # Following the Linux SONAME here.
+  ${INSTALL_NAME_TOOL:-install_name_tool} -id @rpath/librhash.0.dylib ${PREFIX}/lib/librhash.0.dylib
 fi
 
 pushd tests
