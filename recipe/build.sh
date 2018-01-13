574945a17d1a017677d6803edc70ec2f91ba47d6
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-01-13 06:57:48 -0500

    Run tests during the test phase

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,3 @@ elif [[ ${HOST} =~ .*darwin.* ]]; then
   # Following the Linux SONAME here.
   ${INSTALL_NAME_TOOL:-install_name_tool} -id @rpath/librhash.0.dylib ${PREFIX}/lib/librhash.0.dylib
 fi
-
-pushd tests
-  ./test_rhash.sh
-popd
