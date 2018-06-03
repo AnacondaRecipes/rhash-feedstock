85f4e4a3bcb51e514793e3ba4f270e529cac440c
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-06-03 15:43:25 -0500

    Bump version to 1.3.6
    - enable OpenSSL (optimized hash functions) support
    - simplify build process using configure script
    - add run_exports

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+set -x
 
-if [[ ${HOST} =~ .*linux.* ]]; then
-  make ADDCFLAGS="${CFLAGS}" ADDLDFLAGS="${LDFLAGS}" install-lib-static
-  make             PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}" install-shared
-  make -C librhash PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}"         install-lib-shared install-lib-static
-elif [[ ${HOST} =~ .*darwin.* ]]; then
-  make             PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}" install                    install-lib-static
-  make -C librhash PREFIX="" CC="${CC}" CFLAGS="${CFLAGS}"                     dylib
-  cp -a librhash/*.dylib* ${PREFIX}/lib
-  # Following the Linux SONAME here.
+./configure \
+  --enable-openssl \
+  --prefix=$PREFIX \
+  --enable-lib-static \
+  --enable-lib-shared \
+  --extra-cflags="$CFLAGS -I$PREFIX/include" \
+  --extra-ldflags="$LDFLAGS"
+
+make install
+make check
+
+if [[ ${HOST} =~ .*darwin.* ]]; then
   ${INSTALL_NAME_TOOL:-install_name_tool} -id @rpath/librhash.0.dylib ${PREFIX}/lib/librhash.0.dylib
 fi
