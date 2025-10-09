2340c25cbc2ac77f19879f2163638b8d70673b3d
Andrii Osipov <aosipov@anaconda.com>
2025-10-09 04:31:51 -0700

    PKG-9904: rhash 1.4.6

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,6 @@ set -x
 
 if [[ ${target_platform} =~ .*linux*. ]]; then
   RPATH="-Wl,-rpath-link,${PREFIX}/lib"
-elif [[ ${target_platform} == osx-64 ]]; then
-  RPATH="-Wl,-rpath,${PREFIX}/lib"
 fi
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib ${RPATH}"
@@ -12,7 +10,6 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib ${RPATH}"
 ./configure \
   --enable-openssl \
   --prefix=$PREFIX \
-  --enable-lib-static \
   --enable-lib-shared \
   --extra-cflags="$CFLAGS -I$PREFIX/include" \
   --extra-ldflags="$LDFLAGS" \
@@ -22,5 +19,5 @@ make install install-lib-headers install-lib-so-link
 make check
 
 if [[ ${HOST} =~ .*darwin.* ]]; then
-  ${INSTALL_NAME_TOOL:-install_name_tool} -id @rpath/librhash.0.dylib ${PREFIX}/lib/librhash.0.dylib
+  ${INSTALL_NAME_TOOL:-install_name_tool} -id @rpath/librhash.1.dylib ${PREFIX}/lib/librhash.1.dylib
 fi
